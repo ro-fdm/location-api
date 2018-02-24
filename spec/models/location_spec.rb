846fb6cb978cc89fa1b1325d6a0cb0c0a25db131
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2018-02-24 15:15:03 +0100

    location test in red

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:postcode) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:country) }
 end
