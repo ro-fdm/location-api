95261a9fddb1d138e34ca8242a2d99607269b38a
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2018-02-26 20:06:13 +0100

    add control error to geocoding

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -17,19 +17,39 @@ class GeocodingService
     "#{@location.address} #{@location.postcode} #{@location.city} #{@location.country}"
   end
 
+  def parse_url
+    url = "https://maps.googleapis.com/maps/api/geocode/json?" + "address=#{build_address} " + "&key=#{ENV["api_google_maps"]}"
+    URI.escape(url)
+  end
+
   def call_google_maps
-    url       = "https://maps.googleapis.com/maps/api/geocode/json?" + "address=#{build_address} " + "&key=#{ENV["api_google_maps"]}"
-    parse_url = URI.escape(url)
     response  = RestClient.get(parse_url)
-    get_coordinates(response)
+    errors_control(response)
+  end
+
+  def errors_control(response)
+    parse_response  = JSON.parse(response)
+    if parse_response["status"] == "OK"
+      control_partial_match(parse_response)
+      get_coordinates(parse_response)
+    elsif parse_response["status"] == "ZERO_RESULTS"
+      @coordinates[:error] = "address not exist"
+    else
+      @coordinates[:error] = parse_response["status"]
+    end
+  end
+
+  def control_partial_match(response)
+    if response["results"].first["partial_match"]
+      @coordinates[:error] = "possible partial match"
+    end
   end
 
   def get_coordinates(response)
-    parse_response  = JSON.parse(response)
     @coordinates    = {
-                       latitude: parse_response["results"].first["geometry"]["location"]["lat"],
-                       longitude: parse_response["results"].first["geometry"]["location"]["lng"],
-                       formatted_address: parse_response["results"].first["formatted_address"]
+                       latitude: response["results"].first["geometry"]["location"]["lat"],
+                       longitude: response["results"].first["geometry"]["location"]["lng"],
+                       formatted_address: response["results"].first["formatted_address"]
                       }
   end
 
